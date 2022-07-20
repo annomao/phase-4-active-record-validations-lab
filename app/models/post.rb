1b460edc5cdc0f6409eed5d8a15c7a67f7cc650b
annomao <omaoannkerubo@gmail.com>
2022-07-21 02:42:45 +0300

    commit tree 4cb09213cf4fd8919618b0336c0714f6379d0af8

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content , length: { minimum: 250 }
+  validates :summary , length: { maximum: 250 }
+  validates :category, inclusion: ["Fiction", "Non-Fiction"]
+  validate :click_baity_title
+
+  def click_baity_title
+    click_baits = [/Won't Believe/i, /Secret/i, /Top \d/i, /Guess/i ]
+
+    if click_baits.none? {|bait| bait.match title}
+    errors.add(:title, "title must be clickbaity")
+    end
+  end
 end
